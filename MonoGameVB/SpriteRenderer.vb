335d11b8748ff09ae3cfaa4795cb44d8168a709f
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-02-12 05:43:16 -0600

    option strict is on, and dotnet 6

--- MonoGameVB/SpriteRenderer.vb
+++ MonoGameVB/SpriteRenderer.vb
@@ -21,8 +21,8 @@ Public Class SpriteRenderer
     Public Sub Draw(spriteIdentifier As SpriteIdentifier, position As XY(Of Integer), hueIdentifier As HueIdentifier) Implements ISpriteRenderer(Of SpriteIdentifier, HueIdentifier).Draw
         Dim sprite = _spriteManager.GetSprite(spriteIdentifier)
         Dim texture = _textureManager.GetTexture(sprite.TextureIdentifier)
-        Dim sourceRectangle As New Rectangle(sprite.Source.X, sprite.Source.Y, sprite.Size.X, sprite.Size.Y)
-        Dim destinationRectangle As New Rectangle((position.X + sprite.Offset.X) * _scale, (position.Y + sprite.Offset.Y) * _scale, sprite.Size.X * _scale, sprite.Size.Y * _scale)
+        Dim sourceRectangle As New Rectangle(sprite.Source.X, sprite.Source.Y, CInt(sprite.Size.X), CInt(sprite.Size.Y))
+        Dim destinationRectangle As New Rectangle((position.X + sprite.Offset.X) * _scale, (position.Y + sprite.Offset.Y) * _scale, CInt(sprite.Size.X * _scale), CInt(sprite.Size.Y * _scale))
         _spriteBatch.Draw(texture, destinationRectangle, sourceRectangle, _hues(hueIdentifier))
     End Sub
 End Class
