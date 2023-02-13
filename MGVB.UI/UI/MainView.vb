a9fb547b18cbee86346b6edb775c0f6004eba025
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-02-13 09:46:50 -0600

    moar texts

--- MGVB.UI/UI/MainView.vb
+++ MGVB.UI/UI/MainView.vb
@@ -3,14 +3,19 @@
     Public Sub OnUpdate(elapsed As TimeSpan) Implements IView(Of SpriteIdentifier, HueIdentifier, Command).OnUpdate
     End Sub
     Public Sub OnDraw(spriteRenderer As ISpriteRenderer(Of SpriteIdentifier, HueIdentifier)) Implements IView(Of SpriteIdentifier, HueIdentifier, Command).OnDraw
-        Dim text = "SOFTSIDE Presents"
-        Dim x = 0
-        Dim y = 0
+        WriteText(spriteRenderer, 0, 0, "SOFTSIDE Presents", HueIdentifier.RED)
+        WriteText(spriteRenderer, 0, 8, """ALIEN"" Adventure", HueIdentifier.RED)
+        WriteText(spriteRenderer, 0, 16, "WRITTEN By Alan J. Zett", HueIdentifier.RED)
+        WriteText(spriteRenderer, 0, 24, "PORTED By TheGrumpyGameDev", HueIdentifier.RED)
+    End Sub
+
+    Private Sub WriteText(spriteRenderer As ISpriteRenderer(Of SpriteIdentifier, HueIdentifier), x As Integer, y As Integer, text As String, hue As HueIdentifier)
         For Each c In text
-            spriteRenderer.Draw(LetterSource(c), New XY(Of Integer)(x, y), HueIdentifier.RED)
+            spriteRenderer.Draw(LetterSource(c), New XY(Of Integer)(x, y), hue)
             x += 8
         Next
     End Sub
+
     Public Function OnCommand(command As Command) As CommandResult Implements IView(Of SpriteIdentifier, HueIdentifier, Command).OnCommand
         Select Case command
             Case Command.BACK
