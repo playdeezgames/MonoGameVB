618d7bccd43aa86b297451728707edbdfc162292
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-02-17 05:59:54 -0600

    some halting steps towards a grammar

--- MGVB.Business/Grammar.vb
+++ MGVB.Business/Grammar.vb
@@ -2,6 +2,11 @@
     Friend ValidSentences As IReadOnlyList(Of (VerbIdentifier, NounIdentifier?)) =
         New List(Of (VerbIdentifier, NounIdentifier?)) From
         {
-            (VerbIdentifier.North, Nothing)
+            (VerbIdentifier.North, Nothing),
+            (VerbIdentifier.South, Nothing),
+            (VerbIdentifier.East, Nothing),
+            (VerbIdentifier.West, Nothing),
+            (VerbIdentifier.Up, Nothing),
+            (VerbIdentifier.Down, Nothing)
         }
 End Module
